a18db1dfc57cb847142d22e62cab7a57bf18d1e3
AdriOnti <adrian.ontiveros.7e4@itb.cat>
2024-05-12 02:32:28 +0200

    lava and animators

---
+++ RatsVsBats/Assets/Adri/Scripts/AdriDie.cs
@@ -0,0 +1,49 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AdriDie : MonoBehaviour
+{
+    [SerializeField]
+    private bool isDead;
+    [SerializeField]
+    private bool isHit;
+    private int lavaHits;
+    private Animator animator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        lavaHits = 0;
+        //animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.TryGetComponent(out Lava lava))
+        {
+            isHit = true;
+            //animator.SetBool("isHit", true);
+        }
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.TryGetComponent(out Lava lava))
+        {
+            lavaHits++;
+            if (lavaHits > 70 && !isDead)
+            {
+                Debug.Log("u dead");
+                isDead = true;
+                //animator.SetBool("isDying", true);
+            }
+        }
+    }
+}
